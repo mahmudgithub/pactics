a61bd901bda806232e60196a6e53b95c0a7cf599
mahmudgithub <mahmudhossain838@gmail.com>
2021-03-18 09:00:12 +0000

    fetch all countery api datas

--- src/Components/First.js
+++ src/Components/First.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios'
 
 const First = () => {
+    const [country, setcountry] = useState([])
+    useEffect(()=>{
+        const lol=async()=>{
+        await Axios({
+            method:'get',
+            url:'https://restcountries.eu/rest/v2/all'
+        })
+        .then((res)=>{setcountry(res.data)})
+        .catch((err)=>{console.log(err);})
+        }
+        lol()
+    },[])
     return (
         <div>
             hello world, this my first deployed react project 
+            <ol>
+
+           
+            {
+                country.map((item,index)=>{return <li key={index}>{item.name}</li>})
+            }
+             </ol>
         </div>
     )
 }
