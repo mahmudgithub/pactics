01831cd4f4ff97e1d6ebc66be5a594d949c218d7
mahmudgithub <mahmudhossain838@gmail.com>
2021-03-18 08:43:05 +0000

    first deploed reacr project

--- src/Components/First.js
+++ src/Components/First.js
@@ -3,7 +3,7 @@ import React from 'react'
 const First = () => {
     return (
         <div>
-            hello world
+            hello world, this my first deployed react project 
         </div>
     )
 }
