93e660c1e015f4e3c5d9ad2d77ed2d43884b519c
mahmudgithub <mahmudhossain838@gmail.com>
2021-03-22 00:49:41 +0000

    remove some unnecceesary files

--- src/Components/First.js
+++ src/Components/First.js
@@ -1,30 +1,68 @@
+// import React, { useEffect, useState } from 'react'
+// import Axios from 'axios'
+
+// const First = () => {
+//     const [country, setcountry] = useState([])
+//     const [search, setsearch] = useState('')
+//     useEffect(()=>{
+//         const lol=async()=>{
+//         await Axios({
+//             method:'get',
+//             url:'https://restcountries.eu/rest/v2/all'
+//         })
+//         .then((res)=>{setcountry(res.data)})
+//         .catch((err)=>{console.log(err);})
+//         }
+//         lol()
+//     },[])
+//     const update=country.filter((item)=>{
+//         return item.name.toLowerCase().includes(search.toLowerCase())
+//     })
+//     return (
+//         <div>
+//             hello world, this my first deployed react project 
+//             <input type='text' placeholder='search ...' onChange={(e)=>{setsearch(e.target.value)}}/>
+//             <ol>
+//             {
+//                 update.map((item,index)=>{return <li key={index}>{item.name}</li>})
+//             }
+//              </ol>
+//         </div>
+//     )
+// }
+
+// export default First
+
+
+
 import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
+// import MOCK from './Data/MOCK.json'
 
 const First = () => {
     const [country, setcountry] = useState([])
-    const [search, setsearch] = useState('')
+    // const [search, setsearch] = useState('')
     useEffect(()=>{
         const lol=async()=>{
         await Axios({
             method:'get',
-            url:'https://restcountries.eu/rest/v2/all'
+            url:'http://127.0.0.1:8000/api/'
         })
-        .then((res)=>{setcountry(res.data)})
+        .then((res)=>{console.log(res.data); setcountry(res.data)})
         .catch((err)=>{console.log(err);})
         }
         lol()
     },[])
-    const update=country.filter((item)=>{
-        return item.name.toLowerCase().includes(search.toLowerCase())
-    })
+    // const update=country.filter((item)=>{
+    //     return item.name.toLowerCase().includes(search.toLowerCase())
+    // })
     return (
         <div>
             hello world, this my first deployed react project 
-            <input type='text' placeholder='search ...' onChange={(e)=>{setsearch(e.target.value)}}/>
+            {/* <input type='text' placeholder='search ...' onChange={(e)=>{setsearch(e.target.value)}}/> */}
             <ol>
             {
-                update.map((item,index)=>{return <li key={index}>{item.name}</li>})
+                country.map((item,index)=>{return <li key={index}>{item.name}</li>})
             }
              </ol>
         </div>
