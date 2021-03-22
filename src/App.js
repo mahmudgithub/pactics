0557bf217a6ba6623897d99e62bc49ea856cd687
mahmudgithub <mahmudhossain838@gmail.com>
2021-03-22 03:44:09 +0000

    removed some unnecessary files

--- src/App.js
+++ src/App.js
@@ -6,19 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <First />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" / ><First />
+        
       </header>
     </div>
   );
